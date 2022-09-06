814fcc21682dd4ed8afffade61838ecf8cab4421
liga140623 <96514104+liga140623@users.noreply.github.com>
2022-09-06 18:49:54 +0200

    Initial commit

---
+++ soloproject.js
@@ -0,0 +1,38 @@
+
+let countEl = document.getElementById("count-el")
+
+let countEl1 = document.getElementById("count-el1")
+
+let count = 0;
+let count1 = 0;
+
+function add1() {
+    count += 1
+    countEl.innerText = count
+};
+
+function add2() {
+    count += 2
+    countEl.innerText = count
+};
+
+function add3() {
+    count += 3
+    countEl.innerText = count
+};
+
+function add4() {
+    count1 += 1
+    countEl1.innerText = count1
+};
+
+function add5() {
+    count1 += 2
+    countEl1.innerText = count1
+};
+
+function add6() {
+    count1 += 3
+    countEl1.innerText = count1
+};
+
